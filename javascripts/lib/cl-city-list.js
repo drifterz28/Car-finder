73927d5b8992916dd9b540e9efcfc4315034ff2b
Chris Whitney <info@e-commsolution.com>
2014-08-30 21:08:04 -0700

    update cl city list

--- javascripts/lib/cl-city-list.js
+++ javascripts/lib/cl-city-list.js
@@ -11,260 +11,209 @@ define([], function () {
                 {
                     abbr:'al',
                     name: 'Alabama',
-                    cities: []
                 },
                 {
                     abbr:'ak',
                     name: 'Alaska',
-                    cities: []
                 },
                 {
                     abbr:'ar',
                     name: 'Arkansas',
-                    cities: []
                 },
                 {
                     abbr:'az',
                     name: 'Arizona',
-                    cities: []
                 },
                 {
                     abbr:'ca',
                     name: 'California',
-                    cities: []
                 },
                 {
                     abbr:'co',
                     name: 'Colorado',
-                    cities: []
                 },
                 {
                     abbr:'ct',
                     name: 'Connecticut',
-                    cities: []
                 },
                 {
                     abbr:'de',
                     name: 'Delaware',
-                    cities: []
                 },
                 {
                     abbr:'dc',
                     name: 'District of Columbia',
-                    cities: []
                 },
                 {
                     abbr:'fl',
                     name: 'Florida',
-                    cities: []
                 },
                 {
                     abbr:'ga',
                     name: 'Georgia',
-                    cities: []
                 },
                 {
                     abbr:'hi',
                     name: 'Hawaii',
-                    cities: []
                 },
                 {
                     abbr:'id',
                     name: 'Idaho',
-                    cities: []
                 },
                 {
                     abbr:'il',
                     name: 'Illinois',
-                    cities: []
                 },
                 {
                     abbr:'in',
                     name: 'Indiana',
-                    cities: []
                 },
                 {
                     abbr:'ia',
                     name: 'Iowa',
-                    cities: []
                 },
                 {
                     abbr:'ks',
                     name: 'Kansas',
-                    cities: []
                 },
                 {
                     abbr:'ky',
                     name: 'Kentucky',
-                    cities: []
                 },
                 {
                     abbr:'la',
                     name: 'Louisiana',
-                    cities: []
                 },
                 {
                     abbr:'me',
                     name: 'Maine',
-                    cities: []
                 },
                 {
                     abbr:'md',
                     name: 'Maryland',
-                    cities: []
                 },
                 {
                     abbr:'ma',
                     name: 'Massachusetts',
-                    cities: []
                 },
                 {
                     abbr:'mi',
                     name: 'Michigan',
-                    cities: []
                 },
                 {
                     abbr:'mn',
                     name: 'Minnesota',
-                    cities: []
                 },
                 {
                     abbr:'ms',
                     name: 'Mississippi',
-                    cities: []
                 },
                 {
                     abbr:'mo',
                     name: 'Missouri',
-                    cities: []
                 },
                 {
                     abbr:'mt',
                     name: 'Montana',
-                    cities: []
                 },
                 {
                     abbr:'ne',
                     name: 'Nebraska',
-                    cities: []
                 },
                 {
                     abbr:'nv',
                     name: 'Nevada',
-                    cities: []
                 },
                 {
                     abbr:'nh',
                     name: 'New Hampshire',
-                    cities: []
                 },
                 {
                     abbr:'nj',
                     name: 'New Jersey',
-                    cities: []
                 },
                 {
                     abbr:'nm',
                     name: 'New Mexico',
-                    cities: []
                 },
                 {
                     abbr:'ny',
                     name: 'New York',
-                    cities: []
                 },
                 {
                     abbr:'nc',
                     name: 'North Carolina',
-                    cities: []
                 },
                 {
                     abbr:'nd',
                     name: 'North Dakota',
-                    cities: []
                 },
                 {
                     abbr:'oh',
                     name: 'Ohio',
-                    cities: []
                 },
                 {
                     abbr:'ok',
                     name: 'Oklahoma',
-                    cities: []
                 },
                 {
                     abbr:'or',
                     name: 'Oregon',
-                    cities: []
                 },
                 {
                     abbr:'pa',
                     name: 'Pennsylvania',
-                    cities: []
                 },
                 {
                     abbr:'ri',
                     name: 'Rhode Island',
-                    cities: []
                 },
                 {
                     abbr:'sc',
                     name: 'South Carolina',
-                    cities: []
                 },
                 {
                     abbr:'sd',
                     name: 'South Dakota',
-                    cities: []
                 },
                 {
                     abbr:'tn',
                     name: 'Tennessee',
-                    cities: []
                 },
                 {
                     abbr:'tx',
                     name: 'Texas',
-                    cities: []
                 },
                 {
                     abbr:'ut',
                     name: 'Utah',
-                    cities: []
                 },
                 {
                     abbr:'vt',
                     name: 'Vermont',
-                    cities: []
                 },
                 {
                     abbr:'va',
                     name: 'Virginia',
-                    cities: []
                 },
                 {
                     abbr:'wa',
                     name: 'Washington',
-                    cities: []
                 },
                 {
                     abbr:'wi',
                     name: 'Wisconsin',
-                    cities: []
                 },
                 {
                     abbr:'wv',
                     name: 'West Virginia',
-                    cities: []
                 },
                 {
                     abbr:'wy',
                     name: 'Wyoming',
-                    cities: []
                 }
             ],
+            cities: [],
         }
     };
 });
-
